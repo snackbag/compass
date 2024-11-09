2f70ef58e00dd93a7895d79b307e56a0a4693509
JXSnack <jx.snack@gmail.com>
2024-11-09 20:25:47 +0100

    more testing

--- main.go
+++ main.go
@@ -2,13 +2,14 @@ package main
 
 import (
 	"compass/compass"
+	"fmt"
 )
 
 func main() {
 	server := compass.NewServer()
 
 	server.AddRoute("/", func(request compass.Request) string {
-		return "<h1>test</h1>"
+		return fmt.Sprintf("Hey, your IP is %s and you sent a %s request", request.IP, request.Method)
 	})
 
 	server.Start()
