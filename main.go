e59dd96330b081e30d7792f26505419bebbdb537
JXSnack <jx.snack@gmail.com>
2024-11-09 23:01:25 +0100

    updated way of doing stuff

--- main.go
+++ main.go
@@ -8,12 +8,16 @@ import (
 func main() {
 	server := compass.NewServer()
 
-	server.AddRoute("/", func(request compass.Request) string {
-		return fmt.Sprintf("Hey, your IP is %s and you sent a %s request", request.IP, request.Method)
+	server.AddRoute("/", func(request compass.Request) compass.Response {
+		return compass.Text(fmt.Sprintf("Hey, your IP is %s and you sent a %s request", request.IP, request.Method))
 	})
 
-	server.SetNotFoundHandler(func(request compass.Request) string {
-		return "woah, that's not found"
+	server.AddRoute("/test", func(request compass.Request) compass.Response {
+		return compass.Redirect("https://google.com/")
+	})
+
+	server.SetNotFoundHandler(func(request compass.Request) compass.Response {
+		return compass.TextWithCode("woah, that's not found", 404)
 	})
 
 	server.Start()
