404a740a68d409ac23b1509b61a60c49367df71b
JXSnack <jx.snack@gmail.com>
2024-11-09 19:40:00 +0100

    implement new logger

--- compass/main.go
+++ compass/main.go
@@ -7,12 +7,30 @@ import (
 )
 
 type Server struct {
-	Port int
+	Port   int
+	Logger Logger
+}
+
+type Logger interface {
+	Info(message string)
+	Warn(message string)
+	Error(message string)
+
+	Request(method string, ip string, route string, code int, useragent string)
+}
+
+func NewServer() Server {
+	return Server{Port: 3000, Logger: NewLogger()}
+}
+
+func NewLogger() Logger {
+	return &SimpleLogger{}
 }
 
 func (server *Server) Start() {
 	http.HandleFunc("/", getRoot)
 
+	server.Logger.Info(fmt.Sprintf("Server is listening on :%d", server.Port))
 	err := http.ListenAndServe(fmt.Sprintf(":%d", server.Port), nil)
 	if err != nil {
 		panic(err)
