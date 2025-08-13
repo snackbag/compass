465086809706513c7487b1db3dffa2f31ecfeabe
JXSnack <jx.snack@gmail.com>
2025-08-13 19:52:42 +0200

    add logging example

---
+++ examples/basics/02_logging/main.go
@@ -0,0 +1,11 @@
+package main
+
+import "github.com/snackbag/compass"
+
+func main() {
+	server := compass.NewServer()
+
+	server.Logger.Info("I'm simply logging some things")
+	server.Logger.Warn("I warn you about stuff")
+	server.Logger.Error("I am a big red scary text")
+}
