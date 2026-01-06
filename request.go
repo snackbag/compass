5d861bee12dc3f1ebf1ac1a89d5317d884c0db27
JXSnack <jx.snack@gmail.com>
2026-01-06 18:41:42 +0100

    basic request handler

---
+++ request.go
@@ -0,0 +1,34 @@
+package compass
+
+import (
+	"net/http"
+	"net/url"
+	"strings"
+)
+
+type Request struct {
+	Method string
+	URL    *url.URL
+
+	Http *http.Request
+}
+
+func NewRequestFromHttp(r *http.Request) Request {
+	return Request{
+		Method: strings.ToLower(r.Method),
+		URL:    r.URL,
+
+		Http: r,
+	}
+}
+
+func (s *Server) handleRequest(w http.ResponseWriter, r Request) error {
+	w.WriteHeader(200)
+	_, err := w.Write([]byte("OK"))
+	if err != nil {
+		return err
+	}
+
+	s.Logger.Request(r.Http, 200)
+	return nil
+}
