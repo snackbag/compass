37f7c2e9cdaff99e884abc8a51f108c2c5759238
JXSnack <jx.snack@gmail.com>
2026-01-06 19:30:13 +0100

    dynamic route path generation

--- route.go
+++ route.go
@@ -1,1 +1,42 @@
 package compass
+
+import (
+	"regexp"
+	"strings"
+)
+
+var routeParamRegex = regexp.MustCompile("(<.+>)")
+
+type routePart struct {
+	id     string
+	prefix string
+	suffix string
+}
+
+func createParts(path string) []routePart {
+	split := strings.Split(path, "/")
+	parts := make([]routePart, 0)
+
+	for _, raw := range split {
+		if raw == "" {
+			continue
+		}
+
+		match := routeParamRegex.FindStringSubmatch(raw)
+		if match == nil {
+			parts = append(parts, routePart{id: "", prefix: raw, suffix: ""})
+			continue
+		}
+
+		id := match[1]    // name inside <>
+		rawId := match[0] // full <...>
+		idx := strings.Index(raw, rawId)
+
+		prefix := raw[:idx]
+		suffix := raw[idx+len(rawId):]
+
+		parts = append(parts, routePart{id: id, prefix: prefix, suffix: suffix})
+	}
+
+	return parts
+}
