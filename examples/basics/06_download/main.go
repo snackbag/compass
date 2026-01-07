bcda2451ceb349720e53e93f1e0078371bc73d44
JXSnack <jx.snack@gmail.com>
2026-01-07 19:51:05 +0100

    add file download example

---
+++ examples/basics/06_download/main.go
@@ -0,0 +1,17 @@
+package main
+
+import "github.com/snackbag/compass"
+
+func main() {
+	server := compass.NewServer(compass.NewStandardConfiguration())
+
+	server.AddRoute("/", func(request compass.Request) compass.Response {
+		return compass.DownloadBytes("example.txt", []byte("helo ereveryione!!"))
+	})
+
+	server.AddRoute("/file", func(request compass.Request) compass.Response {
+		return compass.DownloadFile("file.png", "example.png")
+	})
+
+	server.MustRun()
+}
