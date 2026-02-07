874784285a3427668f7589833e47da16f571bd8b
JXSnack <jx.snack@gmail.com>
2026-02-07 14:12:01 +0100

    add Kind method to expressions

--- passplate/expr.go
+++ passplate/expr.go
@@ -2,13 +2,27 @@ package passplate
 
 import "fmt"
 
+type ExpressionKind int
+
+const (
+	VariableKind ExpressionKind = iota
+	TextKind
+	BooleanKind
+)
+
 type Expression interface {
 	Eval() string
 	Repr() string
+
+	Kind() ExpressionKind
 }
 
 type VariableExpr struct {
 	Name string
+}
+
+func (e *VariableExpr) Kind() ExpressionKind {
+	return VariableKind
 }
 
 func (e *VariableExpr) Eval() string {
@@ -23,6 +37,10 @@ type TextExpression struct {
 	Value string
 }
 
+func (e *TextExpression) Kind() ExpressionKind {
+	return TextKind
+}
+
 func (e *TextExpression) Eval() string {
 	return e.Value
 }
@@ -34,6 +52,10 @@ func (e *TextExpression) Repr() string {
 type BooleanExpr struct {
 	Left  Expression
 	Right Expression
+}
+
+func (e *BooleanExpr) Kind() ExpressionKind {
+	return BooleanKind
 }
 
 func (e *BooleanExpr) Eval() string {
